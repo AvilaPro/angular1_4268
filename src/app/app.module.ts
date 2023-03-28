00962a5359bac27581ef73017a02173a578510c0
avilapro <mejiasavilaeduardo@gmail.com>
2023-03-28 17:52:35 -0400

    Primera clase - creacion de componente Models.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// COMPONENTES DE LA APLICACION
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
