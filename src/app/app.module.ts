68ac644b9fc4653722b8f9774fde486554d54d33
avilapro <mejiasavilaeduardo@gmail.com>
2023-04-11 15:39:35 -0400

    primer commit- clases 1 y 2 y cap 9 inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,60 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // COMPONENTES DE LA APLICACION
 import { ModelsComponent } from './models/models.component';
+import { MdbComponent } from './mdb/mdb.component';
+
+// COMPONENTES DE MDB LIBRARY
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//COMPONENTES DE MATERIAL ANGULAR LIBRARY
+import { AngMatComponent } from './ang-mat/ang-mat.component';
+import { MatCardModule } from "@angular/material/card";
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModelsComponent
+    ModelsComponent,
+    MdbComponent,
+    AngMatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    FormsModule
   ],
   providers: [],
   schemas: [
--- src/app/models/models.component.ts
+++ src/app/models/models.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-models',
   templateUrl: './models.component.html',
-  styleUrls: ['./models.component.css']
+  styleUrls: ['./models.component.scss']
 })
 export class ModelsComponent {
 
